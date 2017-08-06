6329acfc4fce7c3a2fbb6ae523c273ba490dd787
rifqisyafii <30776214+rifqisyafii@users.noreply.github.com>
2017-08-06 22:55:21 +0700

    Bot php

--- line-bot-sdk-tiny/LINEBotTiny.php
+++ line-bot-sdk-tiny/LINEBotTiny.php
@@ -97,6 +97,23 @@ class LINEBotTiny
             'Authorization: Bearer ' . $this->channelAccessToken,
         );
 
+curl -X POST \
+-H 'Content-Type:application/json' \
+-H 'Authorization: Bearer {ENTER_ACCESS_TOKEN}' \
+-d '{
+    "replyToken":"nHuyWiB7yP5Zw52FIkcQobQuGDXCTA",
+    "messages":[
+        {
+            "type":"text",
+            "text":"Hello, user"
+        },
+        {
+            "type":"text",
+            "text":"May I help you?"
+        }
+    ]
+}' https://api.line.me/v2/bot/message/reply
+        
         $context = stream_context_create(array(
             "http" => array(
                 "method" => "POST",
